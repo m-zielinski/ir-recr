e6d8d23d44254c7df5adcbdf00348aa6a5a394a4
Marek Zieliński <m.zielinski@ideamotive.co>
2021-04-01 12:13:35 +0200

    implement first working solution

--- app/serializer.rb
+++ app/serializer.rb
@@ -1,2 +1,26 @@
 class Serializer
+  def initialize(object)
+    @object = object
+    @@object = @object
+  end
+
+  def serialize
+    object.to_h.map do |attribute, value|
+      [attribute, send(attribute)]
+    end.to_h
+  end
+
+  def self.attribute(name)
+    self.define_method(name) do
+      block_given? ? yield : object.send(name)
+    end
+  end
+
+  def self.object
+    @@object
+  end
+
+  private
+
+  attr_reader :object
 end
